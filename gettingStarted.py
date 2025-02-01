dbe50fe8294f99de0049cf961d5ad3b0db82707c
vianalin13 <lin.viana13@gmail.com>
2025-02-01 17:46:00 -0500

    changed format

--- gettingStarted.py
+++ gettingStarted.py
@@ -3,32 +3,34 @@
 ### Output - The right answer for the specific question.
 
 def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    match question:
-        case "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-            return "pcap"
-        case "Are encoding and encryption the same? - Yes/No":
-            return "No"
-        case "Is it possible to decrypt a message without a key? - Yes/No":
-            return "No"
-        case "Is it possible to decode a message without a key? - Yes/No":
-            return "Yes"
-        case "Is a hashed message supposed to be un-hashed? - Yes/No":
-            return "No"
-        case "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-            nyu_email = "vl2299@nyu.edu"
-            sha256_hash = hashlib.sha256(nyu_email.encode()).hexdigest()
-            return ""
-        case "Is MD5 a secured hashing algorithm? - Yes/No":
-            return "No"
-        case "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-            return "5"
-        case "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-            return "3"
-        case _:
-            # Default case if the question doesn't match any case
-            return "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+    # Students do not have to follow the skeleton for this assignment.
+    # Another way to implement is using a "case" statements similar to C.
+    
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        nyu_email = "vl2299@nyu.edu"
+        sha256_hash = hashlib.sha256(nyu_email.encode()).hexdigest()
+        answer = sha256_hash
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = "5"
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = "3"
+    else:
+        # Default case if the question doesn't match any case
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+    
+    return answer
 # Complete all the questions.
 
 
